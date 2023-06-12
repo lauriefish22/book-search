65b07cadecba6a13967c8e6c9fd1746636b0ea1a
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-06-12 04:24:27 -0700

    mongoose

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://lauriefish22:Fish2893!', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
